adb05e129389b3c6558762bd702e79a25d2218f5
LAB11_USER ON WS24 <L11Y08W24@citulab.net>
2024-05-16 10:28:39 +0800

    Initial commit

---
+++ app/src/main/java/com/example/carrental_homepage/CarRental_login.java
@@ -0,0 +1,14 @@
+package com.example.carrental_homepage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CarRental_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_car_rental_login);
+    }
+}
